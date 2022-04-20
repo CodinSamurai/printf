f9837c7b68f169de14cb911f25cc2be9292518b0
CodinSamurai <96775952+CodinSamurai@users.noreply.github.com>
2022-04-20 05:03:55 -0700

    Update print_oct.c

--- print_oct.c
+++ print_oct.c
@@ -1,41 +1,44 @@
 #include "main.h"
 
 /**
- * print_rot - writes the str in ROT13
- * @arguments: input string
+ * print_oct - prints decimal number in octal
+ * @arguments: input number
  * @buf: buffer pointer
  * @ibuf: index for buffer pointer
  * Return: number of chars printed.
  */
-
-int print_rot(va_list arguments, char *buf, unsigned int ibuf)
+int print_oct(va_list arguments, char *buf, unsigned int ibuf)
 {
-	char alf[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-	char *str;
-	unsigned int i, j, k;
-	char nill[] = "(avyy)";
+	int int_input, i, isnegative, count, first_digit;
+	char *octal, *binary;
 
-	str = va_arg(arguments, char *);
-	if (str == NULL)
+	int_input = va_arg(arguments, int);
+	isnegative = 0;
+	if (int_input == 0)
 	{
-		for (i = 0; nill[i]; i++)
-			ibuf = handl_buf(buf, nill[i], ibuf);
-		return (6);
+		ibuf = handl_buf(buf, '0', ibuf);
+		return (1);
 	}
-	for (i = 0; str[i]; i++)
+	if (int_input < 0)
 	{
-		for (k = j = 0; alf[j]; j++)
+		int_input = (int_input * -1) - 1;
+		isnegative = 1;
+	}
+	binary = malloc(sizeof(char) * (32 + 1));
+	binary = fill_binary_array(binary, int_input, isnegative, 32);
+	octal = malloc(sizeof(char) * (11 + 1));
+	octal = fill_oct_array(binary, octal);
+	for (first_digit = i = count = 0; octal[i]; i++)
+	{
+		if (octal[i] != '0' && first_digit == 0)
+			first_digit = 1;
+		if (first_digit)
 		{
-			if (str[i] == alf[j])
-			{
-				k = 1;
-				ibuf = handl_buf(buf, rot[j], ibuf);
-				break;
-			}
+			ibuf = handl_buf(buf, octal[i], ibuf);
+			count++;
 		}
-		if (k == 0)
-			ibuf = handl_buf(buf, str[i], ibuf);
 	}
-	return (i);
+	free(binary);
+	free(octal);
+	return (count);
 }
